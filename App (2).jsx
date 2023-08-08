739d900ca82e3dd72e23dbc56ed8a49484a61ff7
breezingit <71215996+breezingit@users.noreply.github.com>
2023-08-08 18:13:49 +0530

    Update App (2).jsx

--- App (2).jsx
+++ App (2).jsx
@@ -1,14 +1,13 @@
 const data = {
-  "io_1": [{'a': 1, 'b': 2, 'L': "abc1"}, {'a': 3, 'b': 4, 'L': "abc2"}, {'a': 5, 'b': 6, 'L': "abc3"}],
-  "io_2": [{'a': 1, 'b': 2, 'L': "abc4"}, {'a': 3, 'b': 4, 'L': "abc5"}, {'a': 5, 'b': 6, 'L': "abc6"}],
-  "io_3": [{'a': 3, 'b': 4, 'L': "abc7"}, {'a': 1, 'b': 2, 'L': "abc8"}, {'a': 5, 'b': 6, 'L': "abc9"}],
+  "io_1": [{'a': 1, 'b': 2, 'c': '1', 'd': '2', 'L': "abc1"}, {'a': 3, 'b': 4, 'c': '1', 'd': '2', 'L': "abc2"}, {'a': 5, 'b': 6, 'c': '1', 'd': '2', 'L': "abc3"}],
+  "io_2": [{'a': 1, 'b': 2, 'c': '1', 'd': '2', 'L': "abc4"}, {'a': 3, 'b': 4, 'c': '1', 'd': '2', 'L': "abc5"}, {'a': 5, 'b': 6, 'c': '1', 'd': '2', 'L': "abc6"}],
+  "io_3": [{'a': 3, 'b': 4, 'c': '1', 'd': '2', 'L': "abc7"}, {'a': 1, 'b': 2, 'c': '1', 'd': '2', 'L': "abc8"}, {'a': 5, 'b': 6, 'c': '1', 'd': '2', 'L': "abc9"}],
 };
-
 const output = {};
 
 for (const key in data) {
   data[key].forEach(item => {
-    const { a, b, L } = item;
+    const { a, b, c, d, L } = item;
     const row = `${L} : ${key}`;
     
     if (!output[a]) {
@@ -16,21 +15,31 @@ for (const key in data) {
     }
     
     if (!output[a][b]) {
-      output[a][b] = [];
+      output[a][b] = {};
     }
     
-    output[a][b].push(row);
+    if (!output[a][b][c]) {
+      output[a][b][c] = {};
+    }
+    
+    if (!output[a][b][c][d]) {
+      output[a][b][c][d] = [];
+    }
+    
+    output[a][b][c][d].push(row);
   });
 }
-
-
 
 const result = [];
 
 for (const a in output) {
   for (const b in output[a]) {
-    result.push(Object.fromEntries(output[a][b].map(row => [row.split(' : ')[0], row.split(' : ')[1]])));
+    for (const c in output[a][b]) {
+      for (const d in output[a][b][c]) {
+        result.push(Object.fromEntries(output[a][b][c][d].map(row => [row.split(' : ')[0], row.split(' : ')[1]])));
+      }
+    }
   }
 }
-console.log(result)
 
+console.log(result);
