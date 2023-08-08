b94d0d6a01f3ec9a4052e66ec23cfd8b172a97aa
breezingit <71215996+breezingit@users.noreply.github.com>
2023-08-08 16:37:23 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,63 +1,64 @@
-import './App.css'
-
-import React from 'react';
-
 function areElementsSame(data) {
-  const keys = Object.keys(data);
-  
-  for (let i = 0; i < keys.length - 1; i++) {
-    const key1 = keys[i];
-    const key2 = keys[i + 1];
+    const sortedData = {};
     
-    if (data[key1].length !== data[key2].length) {
-      return false;
+    for (const key in data) {
+      const sortedSubElements = data[key].sort((obj1, obj2) => {
+  const keys1 = Object.keys(obj1).sort();
+  const keys2 = Object.keys(obj2).sort();
+
+  for (let i = 0; i < Math.min(keys1.length, keys2.length); i++) {
+    const key1 = keys1[i];
+    const key2 = keys2[i];
+
+    if (key1 !== key2) {
+      return key1.localeCompare(key2);
     }
-    
-    for (let j = 0; j < data[key1].length; j++) {
-      const element1 = data[key1][j];
-      const element2 = data[key2][j];
-      
-      if (!isObjectEqual(element1, element2)) {
-        return false;
-      }
+
+    const value1 = obj1[key1];
+    const value2 = obj2[key2];
+
+    if (value1 !== value2) {
+      return value1 - value2;
     }
   }
-  
+
+  return keys1.length - keys2.length;
+});
+    
+      sortedData[key] = sortedSubElements;
+    }
+    
+  const sortedDataValues = Object.values(sortedData);
+  console.log(sortedDataValues)
+  const firstElement = sortedDataValues[0];
+
+  for (let i = 1; i < sortedDataValues.length; i++) {
+    if (JSON.stringify(firstElement) !== JSON.stringify(sortedDataValues[i])) {
+      return false;
+    }
+  }
+
   return true;
 }
 
-function isObjectEqual(obj1, obj2) {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-  
-  for (const key of keys1) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-  
-  return true;
-}
+const data1 = {
+  1: [{'a': 1, 'b': 2}, {'c': 1, 'd': 2}, {'e': 1, 'f': 2}],
+  2: [{'a': 1, 'b': 2}, {'c': 1, 'd': 2}, {'e': 1, 'f': 2}],
+  3: [{'c': 1, 'b': 2}, {'c': 1, 'd': 2}, {'e': 1, 'f': 2}],
+};
 
-function App() {
+const data2 = {
+  1: [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}, {'e': 1, 'f': 2}],
+  2: [{'a': 1, 'b': 2}, {'a': 3, 'b': 4}, {'e': 1, 'f': 2}],
+  3: [{'a': 3, 'b': 4}, {'a': 1, 'b': 2}, {'e': 1, 'f': 2}],
+};
+
+
   const data = {
     1: [{'a': 1, 'b': 2}, {'c': 1, 'd': 2}, {'e': 1, 'f': 2}],
     2: [{'a': 1, 'b': 2}, {'c': 1, 'd': 2}, {'e': 1, 'f': 2}],
     3: [{'a': 1, 'b': 2}, {'c': 1, 'd': 2}, {'e': 1, 'f': 2}],
   };
 
-  const result = areElementsSame(data);
-  
-  return (
-    <div>
-      <h1>Are Elements Same? {result ? 'Yes' : 'No'}</h1>
-    </div>
-  );
-}
-
-export default App;
-
+  const result = areElementsSame(data2);
+console.log(result)
